01db9f146cd20c28448c6fb0552ba9ab986451c9
fredlc <fredlefevre@outlook.com>
2019-02-12 17:47:33 -0500

    Initial project setup

---
+++ Chatter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chatter
+//
+//  Created by Fred Lefevre on 2019-02-12.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
